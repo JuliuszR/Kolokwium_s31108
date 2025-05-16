eb8630594ca5a288e9521398dda57f484a7ccf91
Juliusz Rączkowski <s31108@pjwstk.edu.pl>
2025-05-16 15:52:40 +0200

    First init

---
+++ Kolokwium_s31108/Kolokwium_s31108/Program.cs
@@ -0,0 +1,22 @@
+namespace Kolokwium_s31108;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+        builder.Services.AddControllers();
+        builder.Services.AddScoped<IDbService, DbService>();
+        
+        var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+        app.UseAuthorization();
+
+        app.MapControllers();
+
+        app.Run();
+    }
+}
